b5752e5adf4ea4791662282fb4db39a21fc3d0bb
Ray <ray_punkeki@hotmail.com>
2023-09-25 19:31:44 -0500

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'offers-app-u202124061-u202112766';
+}
